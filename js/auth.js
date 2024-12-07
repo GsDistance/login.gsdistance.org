3b17fa109b134b511eed69539a370a60027a57d6
Gokalp Sen <gokalpsen09@gmail.com>
2024-12-07 23:53:08 +0300

    Fix stuff

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,9 @@ window.addEventListener('load', async function() {
     const signInDiv = document.getElementById('sign-in');
     const signUpDiv = document.getElementById('sign-up');
     const switchAuthBtn = document.getElementById('switch-auth');
+    const params = new URLSearchParams(window.location.search);
     let isSignIn = true;
+    
 
     // Mount the sign-in component
     window.Clerk.mountSignIn(signInDiv);
@@ -15,17 +17,25 @@ window.addEventListener('load', async function() {
 
     // Switch between sign-in and sign-up
     switchAuthBtn.addEventListener('click', () => {
-        isSignIn = !isSignIn;
-        signInDiv.style.display = isSignIn ? 'block' : 'none';
-        signUpDiv.style.display = isSignIn ? 'none' : 'block';
-        switchAuthBtn.textContent = isSignIn ? 'Create an account' : 'Back to login';
+        switchAuth();
     });
+
+    if (params.get('signUp') === 'true') {
+        switchAuth();
+    }
 
     // Handle authentication state changes
     window.Clerk.addListener(({ user }) => {
         if (user) {
             // User is signed in, redirect to your main application
-            window.location.href = 'https://www.gsdistance.org/dashboard';
+            window.location.href = 'https://www.gsdistance.org/';
         }
     });
 });
+
+function switchAuth() {
+    isSignIn = !isSignIn;
+        signInDiv.style.display = isSignIn ? 'block' : 'none';
+        signUpDiv.style.display = isSignIn ? 'none' : 'block';
+        switchAuthBtn.textContent = isSignIn ? 'Create an account' : 'Back to login';
+}
